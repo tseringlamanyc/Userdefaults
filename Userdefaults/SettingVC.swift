b7f711b5efd216ce20c0671fb71dd06c720cf1f6
Tsering Lama <tseringlama@pursuit.org>
2020-01-13 11:06:55 -0500

    added labels to the table view controller

---
+++ Userdefaults/SettingVC.swift
@@ -0,0 +1,21 @@
+//
+//  SettingVC.swift
+//  Userdefaults
+//
+//  Created by Tsering Lama on 1/13/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UITableViewController {
+    
+    @IBOutlet weak var measurementText: UILabel!
+    @IBOutlet weak var unitText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
